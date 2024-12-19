8df47c76f3e47cf499c764d7fdcfcd2d578a3107
Piotr Menel <phenomenelik@gmail.com>
2024-12-19 09:19:09 +0100

    fixes from solution files

--- main.py
+++ main.py
@@ -10,12 +10,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill(color="black")
-            pygame.display.flip()
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill(color="black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
