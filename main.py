bfa4a580f1c25adeab278c02e2d0c5c489ce4c6a
Piotr Menel <phenomenelik@gmail.com>
2024-12-19 18:24:44 +0100

    create a player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -7,7 +8,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(
+        x=SCREEN_WIDTH / 2,
+        y=SCREEN_HEIGHT / 2
+    )
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +19,7 @@ def main():
                 return
 
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
---
+++ player.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+
+class Player(CircleShape):
+    def __init__(self, x: int, y: int) -> None:
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(
+            surface=screen,
+            color="white",
+            points=self.triangle(),
+            width=2
+        )
