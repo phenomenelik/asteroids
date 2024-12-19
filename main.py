74123a3cb877ee69d63561cfc479155109b8c375
Piotr Menel <phenomenelik@gmail.com>
2024-12-19 19:12:44 +0100

    moving around

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ def main():
                 return
 
         screen.fill(color="black")
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
